36c1412ac27a08dc56a756b03bc238ccd2442240
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2021-11-28 13:02:13 +0000

    Add files via upload

---
+++ database/menu.js
@@ -0,0 +1,240 @@
+const menunya = (prefix) => {
+	return`┌❁ *INFO ${NamaBot}* ❁
+│👤𝔒𝔴𝔫𝔢𝔯: Rifai
+│💌𝔈𝔪𝔞𝔦𝔩: rifaimuhammadrifaifai@gmail.com
+│🛑𝔜𝔬𝔲𝔱𝔲𝔟𝔢: itsmeR1F4I
+│♥️ℑ𝔫𝔰𝔱𝔞𝔤𝔯𝔞𝔪: -
+│📱𝔊𝔞𝔪𝔢: Minecraft
+│📞𝔑𝔬𝔪𝔬𝔯: 6287834324804
+├━━❁ *OWNER* ❁
+├❥ .addrespon
+├❥ .autorespon 1/0
+├❥ .bc
+├❥ .bcaudio
+├❥ .clearall
+├❥ .creategrup
+├❥ .culik
+├❥ .delcmd
+├❥ .exif
+├❥ .getscmd
+├❥ .jadibot
+├❥ .listcmd
+├❥ .mute
+├❥ .nsfw 1/0
+├❥ .offline
+├❥ .online
+├❥ .public
+├❥ .restart
+├❥ .tobc
+├❥ .self
+├❥ .setbio
+├❥ .setcmd
+├❥ .setprefix
+├❥ .setprofile
+├❥ .shutdown
+├❥ .stopjadibot
+├━━❁ *UPLOAD SW* ❁
+├❥ .spamsw
+├❥ .upswteks
+├❥ .upswlokasi
+├❥ .upswsticker
+├❥ .upswaudio
+├❥ .upswvoice
+├❥ .upswvideo
+├❥  .upswgif
+├❥ .upswimage
+├━━❁ *GRUP* ❁
+├❥ .ad ( reply pesan )
+├❥ .add 628..dst
+├❥ .antibug 1/0
+├❥ .antidelete 1/0
+├❥ .antilink 1/0
+├❥ .caripesan
+├❥ .fitnah
+├❥ .getdeskgc
+├❥ .grup
+├❥ .getpp @tagtarget
+├❥ .hacked
+├❥ .hidetag
+├❥ .infouser (reply pesan)
+├❥ .kontak
+├❥ .kick @tagmember
+├❥ .kik  ( reply pesan )
+├❥ .linkgrup
+├❥ .listadmin
+├❥ .listonline
+├❥ .revoke
+├❥ .setdeskgc
+├❥ .setnamegc
+├❥ .sider
+├❥ .sticktag
+├❥ .spam
+├❥ .tag
+├❥ .tagall
+├❥ .tagme
+├❥ .tospam
+├❥ .totag
+├━━❁ *MAKER* ❁
+├❥ .attp
+├❥ .battlefield
+├❥ .blackbird
+├❥ .codetxt
+├❥ .coffeecup
+├❥ .coffeecup2
+├❥ .dance
+├❥ .express
+├❥ .foliokanan
+├❥ .foliokiri
+├❥ .glitch
+├❥ .glow
+├❥ .goldbutton
+├❥ .golden
+├❥ .googletxt
+├❥ .hallowen
+├❥ .hengker (reply Gambarnya)
+├❥ .hengker2 (reply Gambarnya)
+├❥ .maker2d2
+├❥ .maker2d3
+├❥ .maker2d4
+├❥ .maker3d
+├❥ .maker3d2
+├❥ .maker3d3
+├❥ .maker3d4
+├❥ .matrix
+├❥ .neon
+├❥ .nulis
+├❥ .nuliskiri
+├❥ .nuliskanan
+├❥ .nostonk (reply Gambarnya)
+├❥ .patrick 
+├❥ .pornhub (teks|teks)
+├❥ .qrcode
+├❥ .readmore
+├❥ .rip (reply Gambarnya)
+├❥ .semoji
+├❥ .shareloc
+├❥ .silverbutton
+├❥ .stonk (reply Gambarnya)
+├❥ .spiderman
+├❥ .spongebob (reply Gambarnya)
+├❥ .summer
+├❥ .tahta
+├❥ .text3d
+├❥ .tinyurl
+├❥ .toimg
+├❥ .tomp3
+├❥ .totext
+├❥ .tourl
+├❥ .transformer
+├❥ .ttp
+├❥ .tts
+├❥ .vampire
+├❥ .viewonce
+├❥ .warrior
+├❥ .wooden
+├━━❁ *DOWNLOADER* ❁
+├❥ .dafontdl (link Dafont)
+├❥ .igstory
+├❥ .igdl
+├❥ .mediafire
+├❥ .play
+├❥ .tiktok
+├❥ .ytmp3
+├❥ .ytmp4 
+├❥ .ytdownload 
+├━━❁ *FUN* ❁
+├❥ .artimimpi
+├❥ .bilangangka
+├❥ .bisakah
+├❥ .cekpintar
+├❥ .cekganteng
+├❥ .cekcantik
+├❥ .cekmati
+├❥ .dadu
+├❥ .dare
+├❥ .delttc
+├❥ .fdeface (error)
+├❥ .fitnahpc
+├❥ .hbd
+├❥ .herolist
+├❥ .herodetail
+├❥ .jadian
+├❥ .kapankah
+├❥ .slot
+├❥ .suit
+├❥ .tictactoe
+├❥ .truth
+├━━❁ *STICKER* ❁
+├❥ .sticker
+├❥ .swm
+├❥ .takestick
+├❥ .colong
+├━━❁ *SEARCH* ❁
+├❥ .brainly
+├❥ .dafontsearch (nama Font)
+├❥ .githubstalk
+├❥ .gimage
+├❥ .google
+├❥ .infogempa
+├❥ .lirik
+├❥ .pinterest
+├❥ .playstore
+├❥ .resepmasakan
+├❥ .wiki
+├❥ .ytsearch
+├━━❁ *WAR* ❁
+├❥ .xbug
+├❥ .bugbutton
+├❥ .bugrow
+├❥ .bugcombine
+├❥ .tesbug
+├❥ .bugpc
+├❥ .bugtroli2
+├❥ .bugtroli3
+├❥ .virtex
+├❥ .tovirvid
+├❥ .tovirgam
+├❥ .bugkatalog (perbaikan)
+├❥ .bugloc
+├❥ .buglokasi
+├❥ .x
+├━━❁ *STORAGE* ❁
+├❥ .addimage
+├❥ .addsticker
+├❥ .addvn
+├❥ .delimage
+├❥ .delsticker
+├❥ .delvn
+├❥ .liststicker
+├❥ .listvn
+├❥ .listimage
+├━━❁ *ASUPAN* ❁
+├❥ .asupan
+├━━❁ *TOLLS* ❁
+├❥ .get
+├❥ .kalkulator
+├━━❁ *TENTANG BOT* ❁
+├❥ .listblock
+├❥ .baileys
+├❥ .delete
+├❥ .getcaption
+├❥ .listgrup
+├❥ .owner
+├❥ .q
+├❥ .report (Pesan Errornya)
+├❥ .rules
+├❥ .runtime
+├❥ .sewa
+├❥ .speed
+├❥ .status
+├❥ .tes
+├━━❁ *CONVERT* ❁
+├❥ .balik (reply audio)
+├❥ .bass (reply audio)
+├❥ .detikvn (reply audio)
+├❥ .detikvideo (reply audio)
+├❥ .gemuk (reply audio)
+├❥ .robot (reply audio)
+└━━❁ *${NamaBot}* ❁`
+}
+exports.menunya = menunya
